eb4414555fa42cecc8a4e92278dadde921ecf9f6
Heri Setyawan <herisetyawan233@gmail.com>
2022-11-21 16:38:46 +0700

    progress component: card service

--- components/Element/Card/CardService.tsx
+++ components/Element/Card/CardService.tsx
@@ -1,14 +1,17 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import WebDesign from "../../../assets/img/web.png"
 import Button from "../Button/Index";
-const CardService = () => {
+const CardService = ({ title, image }: {
+  title: string
+  image: StaticImageData
+}) => {
   return (
     <div className='bg-white p-10 rounded-xl w-80 flex flex-col gap-y-5 justify-center items-center shadow-md hover:shadow-xl duration-500'>
       <div className="w-56 h-56 relative">
-        <Image src={WebDesign} alt="" layout={"responsive"} />
+        <Image src={image} alt="" layout={"responsive"} />
       </div>
       <div className="mt-5 text-center w-full">
-        <h1 className="font-bold text-2xl text-green-600">Web Developer</h1>
+        <h1 className="font-bold text-2xl text-green-600">{ title }</h1>
         <p className="mt-5 text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae tempore doloribus consequatur officia illum necessitatibus</p>
       </div>
       <div className="w-full">
