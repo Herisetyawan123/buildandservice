250c2f2fc4cd63409ff81034eaa556eb390d5c19
Heri Setyawan <herisetyawan233@gmail.com>
2022-11-24 20:56:08 +0700

    erd

--- utils/encrypt.ts
+++ utils/encrypt.ts
@@ -1,16 +1,13 @@
 import bcrypt from 'bcrypt';
 
-type hashType = {
-  hash: (password: string) => void;
-  compare: (password: string, encrypt: any) => void;
-};
 
-const hashPassword: hashType = {
-  hash: (password) => {
+
+const hashPassword = {
+  hash: (password : string) => {
     const encrypt = bcrypt.hashSync(password, 10);
     return encrypt;
   },
-  compare: (password, encrypt) => {
+  compare: (password: string, encrypt: string) => {
     const comparePassword = bcrypt.compareSync(password, encrypt);
     return comparePassword;
   },
