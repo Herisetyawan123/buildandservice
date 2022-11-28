3737b99e7f0606a7bfecd3c30c46616690fe748b
Heri Setyawan <herisetyawan233@gmail.com>
2022-11-29 06:12:26 +0700

    feat: add analytics vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+      <Analytics />
+    </>
   )
 }
 
