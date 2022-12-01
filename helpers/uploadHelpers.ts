1ff382c646c236341277e4fe33d7af625cdc3bba
perdanaph <danangputro9@gmail.com>
2022-12-01 23:14:56 +0700

    refactoring: add helper for upload sr:)

--- helpers/uploadHelpers.ts
+++ helpers/uploadHelpers.ts
@@ -1,6 +1,7 @@
 import supabase from '../utils/supabase-service';
 
 const uploadHelpers = {
+  // helpers for image bucket
   uploadImageProduct: async (file: Buffer, filename: string, typefile: string) => {
     const { data, error } = await supabase.storage.from('thumbnail').upload(filename, file, {
       contentType: typefile,
@@ -12,6 +13,14 @@ const uploadHelpers = {
     console.log(imgUrl);
     const { data, error } = await supabase.storage.from('thumbnail').remove([imgUrl]);
 
+    return { data, error };
+  },
+
+  uploadFileSrs: async (file: Buffer, filename: string, typefile: string) => {
+    const { data, error } = await supabase.storage.from('system-request').upload(filename, file, {
+      contentType: typefile,
+      upsert: true,
+    });
     return { data, error };
   },
 };
