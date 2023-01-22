782a37107b26e26a7075c0a1159d7680c5f0aea8
Heri Setyawan <herisetyawan233@gmail.com>
2023-01-22 07:31:28 +0700

    fix: remove menu sale

--- components/Parts/Navbar/Index.tsx
+++ components/Parts/Navbar/Index.tsx
@@ -7,13 +7,10 @@ const Navbar = () => {
     return (
         <header className="w-full h-20">
             <nav className="w-[90%] sm:max-w-6xl mx-auto flex h-full items-center font-medium justify-between">
-                <div className="flex items-center">
-
-                    <Image src={Logo} objectFit={'cover'} height={48} width={68} alt="alt" />
-      
+                <Image src={Logo} objectFit={'cover'} height={48} width={68} alt="alt" />
+                <div className="flex items-center">      
                     <div className="ml-20 space-x-20 hidden lg:block ">
                         <Link href={'/'}>Home</Link>
-                        <Link href={'/'}>Sale</Link>
                         <Link href={'/'}>Source</Link>
                         <Link href={'/'}>Service</Link>
                         <Link href={'/team'}>Our Teams</Link>
